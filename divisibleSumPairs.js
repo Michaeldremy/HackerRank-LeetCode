5dd3ce6240a22ceb9e0d19d458310a0b49236d94
Michaeldremy <michaeldremy@gmail.com>
2020-06-10 07:06:23 -0700

    Updated an algo

--- divisibleSumPairs.js
+++ divisibleSumPairs.js
@@ -1,1 +1,18 @@
 // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+function divisibleSumPairs(n, k, ar) {
+    let count = 0;
+    let idx1 = 0;
+    let idx2 = 1;
+    while (idx1 < n - 1) {
+        if((ar[idx1] + ar[idx2]) % k === 0) {
+            count++;
+        }
+        idx2++;
+        if (idx2 === n) {
+            idx1++;
+            idx2 = idx1 + 1;
+        }
+    }
+    return count;
+    }
