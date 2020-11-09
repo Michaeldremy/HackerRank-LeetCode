404931a696be9efc2aa4f9303d3533c1ff188bac
Michaeldremy <michaeldremy@gmail.com>
2020-11-09 11:18:59 -0800

    709 - LeetCode

---
+++ LeetCode-Algos/709_toLowerCase.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/to-lower-case/
+
+
+// we can just return str.toLowerCase()
+
+
+var toLowerCase = function(str) {
+  return str.toLowerCase();
+};
+
+console.log(toLowerCase("Hello"))
