52c73452d18dbba927fbcad4a53ceb1720f55cdf
Michaeldremy <michaeldremy@gmail.com>
2020-11-10 20:55:48 -0800

    7Kyu_fixStringCase - CodeWars

---
+++ CodeWars-Algos/7Kyu_fixStringCase.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/javascript
+
+
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase 
+// letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+const solve = (s) => {
+  let upperCount = 0;
+  let lowerCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toLowerCase()) {
+      lowerCount++;
+    } else {
+      upperCount++;
+    }
+  }
+  if (lowerCount >= upperCount) {
+    return s.toLowerCase()
+  }
+  else if (upperCount > lowerCount) {
+    return s.toUpperCase()
+  }
+}
+
+console.log(solve("coDEEEe"))
