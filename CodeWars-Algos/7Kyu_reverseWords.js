cfa68e723e2e3f9c5f85935ea5a6ad0f09ea23a3
Michaeldremy <michaeldremy@gmail.com>
2020-11-10 20:45:20 -0800

    7Kyu_reverseWords - CodeWars

---
+++ CodeWars-Algos/7Kyu_reverseWords.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ").trim();
+}
+
+console.log(reverseWords("The greatest victory is that which requires no battle"));
